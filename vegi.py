fb989a92fb791075fea077b83faae5641cb277bc
Kumar Gaurav <129366506+gaurav7979077511@users.noreply.github.com>
2025-08-05 22:38:41 +0530

    Update vegi.py

--- vegi.py
+++ vegi.py
@@ -314,6 +314,8 @@ else:
         total_investment = govind_total_investment + gaurav_total_investment + Investment_Credit_Bank
         total_expense = govind_total_expense + gaurav_total_expense
 
+        st.write(gaurav_total_collection , gaurav_total_expense , gaurav_collection_credit, gaurav_settlement_debit, gaurav_settlement_credit)
+        st.write(govind_total_collection , govind_total_expense , govind_collection_credit, govind_settlement_debit, govind_settlement_credit)
   
         remaining_fund_gaurav= gaurav_total_collection - gaurav_total_expense - gaurav_collection_credit + gaurav_settlement_debit - gaurav_settlement_credit
         remaining_fund_govind= govind_total_collection - govind_total_expense - govind_collection_credit + govind_settlement_debit - govind_settlement_credit
