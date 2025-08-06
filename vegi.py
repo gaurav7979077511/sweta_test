1d7765dc677ebe8d33429c3a5f7657e4e1b584a4
Kumar Gaurav <129366506+gaurav7979077511@users.noreply.github.com>
2025-08-06 12:18:11 +0530

    Update vegi.py

--- vegi.py
+++ vegi.py
@@ -707,6 +707,8 @@ else:
         debit_mask = bank_df["Transaction Type"].str.lower().str.contains("debit")
         st.write("credit_mask",credit_mask)
         st.write("debit_mask",debit_mask)
+        st.write("Columns:", bank_df.columns.tolist())
+
     
         total_credit = bank_df.loc[credit_mask, "Amount"].sum()
         total_debit = bank_df.loc[debit_mask, "Amount"].sum()
