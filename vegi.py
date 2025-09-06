25baac8fb244b0f53379eff879cac813b161d570
Kumar Gaurav <129366506+gaurav7979077511@users.noreply.github.com>
2025-09-06 22:52:25 +0530

    Update vegi.py

--- vegi.py
+++ vegi.py
@@ -448,7 +448,7 @@ else:
                 "",
                 ["1 Week", "1 Month", "3 Months", "6 Months", "1 Year", "3 Years", "5 Years", "Max"],
                 horizontal=True,
-                index =7
+                index =2
             )
         
         # Determine the date range based on selection
@@ -501,7 +501,7 @@ else:
         tz = pytz.timezone("Asia/Kolkata")
         now = datetime.now(tz)
         latest_date = date.today()
-        yesterday = latest_date - timedelta(days=1)
+        yesterday = latest_date - timedelta(days=0)
         cur_hour = now.hour
         # If current time is after 4 PM, include today in the date range, else only till yesterday
         if cur_hour >= 16:
@@ -1127,7 +1127,7 @@ else:
                 "",  # Remove label
                 ["1 Week", "1 Month", "3 Months", "6 Months", "1 Year", "3 Years", "5 Years", "Max"],
                 horizontal=True,
-                index=7
+                index=2
             )
         
         # === FILTER BASED ON SELECTION ===
